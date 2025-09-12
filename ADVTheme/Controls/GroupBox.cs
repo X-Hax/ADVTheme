3e5b58b167088389cdcab78daff9111630896bbf
ItsEasyActually <darkspines35@gmail.com>
2025-09-12 12:15:44 -0400

    Added GroupBox custom control.

---
+++ ADVTheme/Controls/GroupBox.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADVTheme.Controls
+{
+	public class GroupBox : HeaderedContentControl
+	{
+		public static readonly StyledProperty<IBrush?> HeaderBackgroundProperty =
+			AvaloniaProperty.Register<GroupBox, IBrush>(nameof(HeaderBackground));
+
+		public IBrush? HeaderBackground
+		{
+			get { return (IBrush)GetValue(HeaderBackgroundProperty); }
+			set { SetValue(HeaderBackgroundProperty, value); }
+		}
+
+		public GroupBox() 
+		{
+			
+		}
+	}
+}
